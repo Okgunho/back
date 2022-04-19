d9cb0c2354672d87c766061c1ba3c15224008492
옥건호 <82382500+Okgunho@users.noreply.github.com>
2022-04-19 17:54:28 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -15,10 +15,13 @@ function login() {
 
     fetch("/login", {
         method: "POST",
-        heafer: {
+        headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(req)
-    });
+        body: JSON.stringify(req),
+    }).then((res) => res.json())
+        .then((res) => {
+
+        });
 }
 
